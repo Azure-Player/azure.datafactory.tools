0bc36dee464f7beea138764b7f42ac62a8cffd8b
Kamil <kamil@nowinski.net>
2020-04-13 19:51:08 +0100

    Deployment of Self-Hosted Integration Runtimes

--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [0.4.0] - 2020-04-13
+### Added
+* Support (deployment) of Integration Runtimes type of connections (inc. Self-Hosted, Managed)
+
 ## [0.3.0] - 2020-04-11
 ### Added
 * Support (deployment) of triggers, including stop/start
--- private/classes.ps1
+++ private/classes.ps1
@@ -6,11 +6,12 @@ class Adf {
     [System.Collections.ArrayList] $DataSets = @{}
     [System.Collections.ArrayList] $DataFlows = @{}
     [System.Collections.ArrayList] $Triggers = @{}
+    [System.Collections.ArrayList] $IntegrationRuntimes = @{}
     [string] $Location = ""
 
     [System.Collections.ArrayList] AllObjects()
     {
-        return $this.LinkedServices + $this.Pipelines + $this.DataSets + $this.DataFlows + $this.Triggers
+        return $this.LinkedServices + $this.Pipelines + $this.DataSets + $this.DataFlows + $this.Triggers + $this.IntegrationRuntimes
     }
 }
 
