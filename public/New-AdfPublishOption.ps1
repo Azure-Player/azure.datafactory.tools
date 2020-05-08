3064a58837a36502d70c84cbe1f3368df7dfce65
Kamil <kamil@nowinski.net>
2020-05-08 15:55:52 +0100

    Added Comment-Based Help for public functions

--- public/New-AdfPublishOption.ps1
+++ public/New-AdfPublishOption.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+Creates an instance of objects with options for publishing ADF.
+
+.DESCRIPTION
+Creates an instance of objects with options for publishing ADF.
+Use it if you want specify particular behaviour during publish operation.
+
+.EXAMPLE
+$opt = New-AdfPublishOption
+$opt.Includes.Add("pipeline.Copy*", "")
+$opt.DeleteNotInSource = $false
+Publish-AdfV2FromJson -RootFolder "$RootFolder" -ResourceGroupName "$ResourceGroupName" -DataFactoryName "$DataFactoryName" -Location "$Location" -Stage "UAT" -Option $opt
+
+.EXAMPLE
+$opt = New-AdfPublishOption
+$opt.DeleteNotInSource = $false
+$opt.StopStartTriggers = $false
+Publish-AdfV2FromJson -RootFolder "$RootFolder" -ResourceGroupName "$ResourceGroupName" -DataFactoryName "$DataFactoryName" -Location "$Location" -Stage "UAT" -Option $opt
+
+.LINK
+Online version: https://github.com/SQLPlayer/azure.datafactory.tools/
+#>
 function New-AdfPublishOption {
     [CmdletBinding()]
     param (    )
