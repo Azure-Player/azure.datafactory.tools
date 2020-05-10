188da995c4273db8612ba3630c88e13bcfa31eac
Liam Dunphy <liam@liamejd.co.uk>
2020-05-11 00:54:01 +0100

    Test-Path to check if dir exist

--- test/Import-AdfFromFolder.Tests.ps1
+++ test/Import-AdfFromFolder.Tests.ps1
@@ -58,42 +58,42 @@ InModuleScope azure.datafactory.tools {
                 $script:result.Pipelines.GetType() | Should -Be 'System.Collections.ArrayList'
                 $cnt = 0
                 $folder = Join-Path -Path "$env:ADF_ExampleCode" -ChildPath "pipeline"
-                if ($folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
+                if (Test-Path $folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
                 $script:result.Pipelines.Count | Should -Be $cnt
             }
             It 'Should contains LinkedServices prop as ArrayList and # of items matches' {
                 $script:result.LinkedServices.GetType() | Should -Be 'System.Collections.ArrayList'
                 $cnt = 0
                 $folder = Join-Path -Path "$env:ADF_ExampleCode" -ChildPath "LinkedService"
-                if ($folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
+                if (Test-Path $folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
                 $script:result.LinkedServices.Count | Should -Be $cnt
             }
             It 'Should contains DataSets prop as ArrayList and # of items matches' {
                 $script:result.DataSets.GetType() | Should -Be 'System.Collections.ArrayList'
                 $cnt = 0
                 $folder = Join-Path -Path "$env:ADF_ExampleCode" -ChildPath "DataSet"
-                if ($folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
+                if (Test-Path $folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
                 $script:result.DataSets.Count | Should -Be $cnt
             }
             It 'Should contains DataFlows prop as ArrayList and # of items matches' {
                 $script:result.DataFlows.GetType() | Should -Be 'System.Collections.ArrayList'
                 $cnt = 0
                 $folder = Join-Path -Path "$env:ADF_ExampleCode" -ChildPath "DataFlow"
-                if ($folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
+                if (Test-Path $folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
                 $script:result.DataFlows.Count | Should -Be $cnt
             }
             It 'Should contains Triggers prop as ArrayList and # of items matches' {
                 $script:result.Triggers.GetType() | Should -Be 'System.Collections.ArrayList'
                 $cnt = 0
                 $folder = Join-Path -Path "$env:ADF_ExampleCode" -ChildPath "Trigger"
-                if ($folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
+                if (Test-Path $folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
                 $script:result.Triggers.Count | Should -Be $cnt
             }
             It 'Should contains IntegrationRuntimes prop as ArrayList and # of items matches' {
                 $script:result.IntegrationRuntimes.GetType() | Should -Be 'System.Collections.ArrayList'
                 $cnt = 0
                 $folder = Join-Path -Path "$env:ADF_ExampleCode" -ChildPath "IntegrationRuntime"
-                if ($folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
+                if (Test-Path $folder) { $cnt = ($folder | Get-ChildItem -Recurse:$false -Filter "*.json" | Measure-Object).Count }
                 $script:result.IntegrationRuntimes.Count | Should -Be $cnt
             }
 
