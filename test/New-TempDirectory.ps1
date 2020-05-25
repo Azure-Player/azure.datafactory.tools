25473ef22d54bb72761b9a3dde80c661a25758ca
Kamil <kamil@nowinski.net>
2020-05-25 16:42:18 +0100

    Added first set of unit tests

---
+++ test/New-TempDirectory.ps1
@@ -0,0 +1,5 @@
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    $name = [System.IO.Path]::GetRandomFileName()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
