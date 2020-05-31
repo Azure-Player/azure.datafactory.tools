28e635b7ac4b6d73f6821abe1e519a46f01fb9c7
Kamil <kamil@nowinski.net>
2020-05-31 16:47:49 +0100

    Import module locally

--- test/!RunAllTests.ps1
+++ test/!RunAllTests.ps1
@@ -8,18 +8,18 @@ Set-Location "$folder"
 Get-Location | Out-Host
 
 # Add the module location to the value of the PSModulePath environment variable
-$p = [Environment]::GetEnvironmentVariable("PSModulePath")
-$p += ";$folder"
-[Environment]::SetEnvironmentVariable("PSModulePath", $p)
+#$p = [Environment]::GetEnvironmentVariable("PSModulePath")
+#$p += ";$folder"
+#[Environment]::SetEnvironmentVariable("PSModulePath", $p)
 
 Write-Host "Installing PS modules..."
 Install-Module 'Az.DataFactory' -Force -MinimumVersion 1.8.0
 Install-Module 'PSScriptAnalyzer' -Force
 Install-Module 'Pester' -Force
-Install-Module 'azure.datafactory.tools'
 Import-Module 'Pester'
 Import-Module 'PSScriptAnalyzer'
 Import-Module 'Az.DataFactory'
+Import-Module "$folder\azure.datafactory.tools.psd1"
 Get-Module | Out-Host
 
 $env:ADF_ExampleCode = "$folder\test\BigFactorySample2\"
