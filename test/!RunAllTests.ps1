0746350d79558c8bae7b9c84022f91b3c346ec80
Kamil <kamil@nowinski.net>
2020-05-31 16:40:01 +0100

    Add folder param

--- test/!RunAllTests.ps1
+++ test/!RunAllTests.ps1
@@ -1,21 +1,26 @@
-Write-Host "Setting new location: $(system.defaultworkingdirectory)"
-Set-Location "$(system.defaultworkingdirectory)"
+Param(
+    [Parameter(Mandatory)]
+    [string]$folder
+)
+
+Write-Host "Setting new location: $folder"
+Set-Location "$folder"
 Get-Location | Out-Host
 
 # Add the module location to the value of the PSModulePath environment variable
 $p = [Environment]::GetEnvironmentVariable("PSModulePath")
-$p += ";$(system.defaultworkingdirectory)"
+$p += ";$folder"
 [Environment]::SetEnvironmentVariable("PSModulePath", $p)
 
 Write-Host "Installing PS modules..."
 Install-Module 'Az.DataFactory' -Force -MinimumVersion 1.8.0
 Install-Module 'PSScriptAnalyzer' -Force
 Install-Module 'Pester' -Force
-Install-Module '$(system.defaultworkingdirectory)'
+Install-Module 'azure.datafactory.tools'
 Import-Module 'Pester'
 Import-Module 'PSScriptAnalyzer'
 Import-Module 'Az.DataFactory'
 Get-Module | Out-Host
 
-$env:ADF_ExampleCode = "$(system.defaultworkingdirectory)\test\BigFactorySample2\"
-Invoke-Pester -Script "$(system.defaultworkingdirectory)\test\*.Tests.ps1" -EnableExit -OutputFile "TEST-Results.xml" -OutputFormat NUnitXml
+$env:ADF_ExampleCode = "$folder\test\BigFactorySample2\"
+Invoke-Pester -Script "$folder\test\*.Tests.ps1" -EnableExit -OutputFile "TEST-Results.xml" -OutputFormat NUnitXml
