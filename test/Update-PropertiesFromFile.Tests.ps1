a41cad31d1e31a5446286335b6b27f25fc17c356
Kamil <kamil@nowinski.net>
2020-12-21 20:27:32 +0000

    Tests for FailsWhenConfigItemNotFound option #59

--- test/Update-PropertiesFromFile.Tests.ps1
+++ test/Update-PropertiesFromFile.Tests.ps1
@@ -1,5 +1,5 @@
 BeforeDiscovery {
-    $ModuleRootPath = (Get-Location)
+    $ModuleRootPath = (Get-Location) | Split-Path -Parent
     $moduleManifestName = 'azure.datafactory.tools.psd1'
     $moduleManifestPath = Join-Path -Path $ModuleRootPath -ChildPath $moduleManifestName
 
@@ -9,13 +9,14 @@ BeforeDiscovery {
 InModuleScope azure.datafactory.tools {
     #$testHelperPath = $PSScriptRoot | Split-Path -Parent | Join-Path -ChildPath 'TestHelper'
     #Import-Module -Name $testHelperPath -Force
-    . ".\test\New-TempDirectory.ps1"
+    . ".\New-TempDirectory.ps1"
 
     # Variables for use in tests
     $script:SrcFolder = $env:ADF_ExampleCode
     $script:TmpFolder = (New-TemporaryDirectory).FullName
     $script:RootFolder = Join-Path -Path $script:TmpFolder -ChildPath (Split-Path -Path $script:SrcFolder -Leaf)
     $script:DeploymentFolder = Join-Path -Path $script:RootFolder -ChildPath "deployment"
+    $script:ConfigFolder = Join-Path -Path $script:SrcFolder -ChildPath "deployment"
 
     Copy-Item -Path "$SrcFolder" -Destination "$TmpFolder" -Filter "*.*" -Recurse:$true -Force 
     #Invoke-Expression "explorer.exe '$TmpFolder'"
@@ -247,6 +248,55 @@ InModuleScope azure.datafactory.tools {
 
 
     } 
+
+
+    Describe 'Update-PropertiesFromFile with JSON' -Tag 'Unit','private' {
+        Context 'When called with FailsWhenConfigItemNotFound = $true' {
+            It 'Should not throw when object exists' {
+                {
+                    $script:adf = Import-AdfFromFolder -FactoryName "xyz" -RootFolder "$RootFolder"
+                    $script:option = New-AdfPublishOption
+                    $option.FailsWhenConfigItemNotFound = $true
+                    $script:adf.PublishOptions = $option
+                    Update-PropertiesFromFile -Adf $adf -stage ( Join-Path -Path $script:ConfigFolder -ChildPath "config-c100.csv" )
+                } | Should -Not -Throw
+            }
+            It 'Should throw when object is missing' {
+                {
+                    $script:adf = Import-AdfFromFolder -FactoryName "xyz" -RootFolder "$RootFolder"
+                    $script:option = New-AdfPublishOption
+                    $option.FailsWhenConfigItemNotFound = $true
+                    $script:adf.PublishOptions = $option
+                    Update-PropertiesFromFile -Adf $adf -stage ( Join-Path -Path $script:ConfigFolder -ChildPath "config-missing.csv" )
+                } | Should -Throw
+            }
+        }
+
+        Context 'When called with FailsWhenConfigItemNotFound = $false' {
+            It 'Should not throw when object exists' {
+                {
+                    $script:adf = Import-AdfFromFolder -FactoryName "xyz" -RootFolder "$RootFolder"
+                    $script:option = New-AdfPublishOption
+                    $option.FailsWhenConfigItemNotFound = $false
+                    $script:adf.PublishOptions = $option
+                    Update-PropertiesFromFile -Adf $adf -stage ( Join-Path -Path $script:ConfigFolder -ChildPath "config-c100.csv" )
+                } | Should -Not -Throw
+            }
+             It 'Should not throw when object is missing' {
+                {
+                    $script:adf = Import-AdfFromFolder -FactoryName "xyz" -RootFolder "$RootFolder"
+                    $script:option = New-AdfPublishOption
+                    $option.FailsWhenConfigItemNotFound = $false
+                    $script:adf.PublishOptions = $option
+                    Update-PropertiesFromFile -Adf $adf -stage ( Join-Path -Path $script:ConfigFolder -ChildPath "config-missing.csv" )
+                } | Should -Not -Throw
+             }
+        }
+
+
+
+    }
+
 
 
 }
