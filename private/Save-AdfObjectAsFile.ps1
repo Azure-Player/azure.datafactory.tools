46d78919211dffc2d93a04b752c4996baa607b4e
Kamil <kamil@nowinski.net>
2021-01-20 22:23:17 +0000

    Fixed JSON files corruption for very deeply contained nodes #80

--- private/Save-AdfObjectAsFile.ps1
+++ private/Save-AdfObjectAsFile.ps1
@@ -7,7 +7,7 @@ function Save-AdfObjectAsFile {
     $newFileName = Join-Path $obj.Adf.Location "$($obj.Type)\~$($obj.Name).json"
     Write-Debug "Writing file: $newFileName"
 
-    $output = ($obj.Body | ConvertTo-Json -Compress:$true -Depth 15)
+    $output = ($obj.Body | ConvertTo-Json -Compress:$true -Depth 100)
     $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
     [IO.File]::WriteAllLines($newFileName, $output, $Utf8NoBomEncoding)
 
