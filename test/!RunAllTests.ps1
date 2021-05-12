2ee17dfae5622901a946a08706a55bf4cc6dd9eb
Niall Langley <niall.langley@workingwithcostain.com>
2021-05-12 15:46:38 +0100

    Tweaks to make tests easier to run during development - Fixed some tests causing errors - Only load modules once for any given PowerShell session - Added params to control excluding integration tests (dependent on Azure) or only inlcuding certain test files. Defaults give original behaviour. - Shell location is returned at end of test script - VSCode debug profile added - Git ignore for test results file

--- test/!RunAllTests.ps1
+++ test/!RunAllTests.ps1
@@ -1,11 +1,17 @@
 Param(
     [Parameter(Mandatory)]
-    [string]$folder
+    [string]$folder,
+
+    [Parameter(Mandatory=$false)]
+    [string]$TestFilenameFilter = "*",
+
+    [Parameter(Mandatory=$false)]
+    [Switch]$LocalTestsOnly
 )
 # $folder = 'X:\!WORK\GitHub\!SQLPlayer\azure.datafactory.tools\test'
 
 Write-Host "Setting new location: $folder"
-Set-Location "$folder"
+Push-Location "$folder"
 Get-Location | Out-Host
 
 # Add the module location to the value of the PSModulePath environment variable
@@ -16,13 +22,20 @@ Get-Location | Out-Host
 Get-PSRepository
 
 Write-Host "Installing PS modules..."
-Install-Module 'Az.DataFactory' -Force -MinimumVersion 1.8.0 -Repository 'PSGallery'
-Install-Module 'PSScriptAnalyzer' -Force
-Install-Module 'Pester' -Force -MinimumVersion 5.1.1
-Import-Module 'Pester'
-Import-Module 'PSScriptAnalyzer'
-Import-Module 'Az.DataFactory'
-Import-Module "$folder\azure.datafactory.tools.psd1"
+# Set a process scoped flag so we can run tests while developing without waiting for modules to load again
+if ($null -eq [Environment]::GetEnvironmentVariable("azure.datafactory.tools.unitTestInstalledModules", 'Process')){
+    [Environment]::SetEnvironmentVariable("azure.datafactory.tools.unitTestInstalledModules", $false, 'Process');
+}
+if ([Environment]::GetEnvironmentVariable("azure.datafactory.tools.unitTestInstalledModules", 'Process') -eq $false){
+    Install-Module 'Az.DataFactory' -Force -MinimumVersion 1.8.0 -Repository 'PSGallery'
+    Install-Module 'PSScriptAnalyzer' -Force
+    Install-Module 'Pester' -Force -MinimumVersion 5.1.1
+    Import-Module 'Pester'
+    Import-Module 'PSScriptAnalyzer'
+    Import-Module 'Az.DataFactory'
+    Import-Module "$folder\azure.datafactory.tools.psd1"
+    [Environment]::SetEnvironmentVariable("azure.datafactory.tools.unitTestInstalledModules", $true, 'Process');
+}
 Get-Module | Out-Host
 
 #v4
@@ -32,31 +45,34 @@ Get-Module | Out-Host
 #Invoke-Pester -Script "$testFile" -EnableExit -OutputFile "TEST-Results.xml" -OutputFormat NUnitXml
 
 #v5
-cls
-$r = $null
-$VerbosePreference = 'SilentlyContinue'
-$ErrorActionPreference = 'Stop'     #Important!!!
+#cls
+try{
+    $r = $null
+    $VerbosePreference = 'SilentlyContinue'
+    $ErrorActionPreference = 'Stop'     #Important!!!
 
-#Set-Location "X:\!WORK\GitHub\!SQLPlayer\azure.datafactory.tools\test"
-$configuration = [PesterConfiguration]::Default
-$configuration.Run.Exit = $true
-$configuration.Should.ErrorAction = 'Continue'
-$configuration.CodeCoverage.Enabled = $false
-$configuration.TestResult.OutputFormat = "NUnitXml"
-$configuration.TestResult.OutputPath = "TEST-Results.xml"
-$configuration.TestResult.Enabled = $true
-$configuration.Output.Verbosity = 'Detailed'
-#$configuration.Filter.ExcludeTag = 'Integration'
-$configuration.Run.Path = "$folder"
-$configuration.Run.TestExtension = "*.Tests.ps1"
-$configuration.Run.PassThru = $true
-$r = Invoke-Pester -Configuration $configuration
-$r.Result
+    #Set-Location "X:\!WORK\GitHub\!SQLPlayer\azure.datafactory.tools\test"
+    $configuration = [PesterConfiguration]::Default
+    $configuration.Run.Exit = $true
+    $configuration.Should.ErrorAction = 'Continue'
+    $configuration.CodeCoverage.Enabled = $false
+    $configuration.TestResult.OutputFormat = "NUnitXml"
+    $configuration.TestResult.OutputPath = "TEST-Results.xml"
+    $configuration.TestResult.Enabled = $true
+    $configuration.Output.Verbosity = 'Detailed'
+    if ($LocalTestsOnly) {
+        $configuration.Filter.ExcludeTag = 'Integration'
+    }
+    $configuration.Run.Path = "$folder"
+    $configuration.Run.TestExtension = "$TestFilenameFilter.Tests.ps1"
+    $configuration.Run.PassThru = $true
+    $r = Invoke-Pester -Configuration $configuration
+    $r.Result
 
-#$r.Failed
-#$r.Passed[0]
-$r.Failed | Format-Table -Property Result, StartLine, Duration, Path
+    #$r.Failed
+    #$r.Passed[0]
+    $r.Failed | Format-Table -Property Result, StartLine, Duration, Path
 
-
-
-
+} finally {
+    Pop-Location
+}
