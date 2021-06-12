5d1dcd088fab76a265a3f9c9a96bd13532305857
Kamil <kamil@nowinski.net>
2021-06-12 22:19:40 +0100

    Find-RefObject fixed

--- private/Get-ReferencedObjects.ps1
+++ private/Get-ReferencedObjects.ps1
@@ -1,23 +1,38 @@
 function Find-RefObject($node, $list) {
+    #Write-Verbose "Type = $($node.GetType().Name)"
+
+    $script:ind++
+    if ($null -eq $node) { $script:ind--; return }
+    if ($node.GetType().Name -notin ('Object[]','PSCustomObject')) { $script:ind--; return }
     if ($null -ne $node.PSobject -and $node.PSobject.Properties.Name -contains 'referenceName' -and $node.PSobject.Properties.Name -contains 'type') {
         [string] $type = $node.type
         if ($type.EndsWith('Reference')) {
             $type = $type.Substring(0, $type.Length-9)
-            #Write-Host "$type.$($node.referenceName)"
+            #Write-Verbose "$type.$($node.referenceName)"
             $list.Add("$type.$($node.referenceName)") | Out-Null
         }
     }
-    
-    $m = Get-Member -InputObject $node -MemberType 'NoteProperty'
-    $m | ForEach-Object {
-        $name = $_.Name
-        #Write-Verbose "Checking: $name"
-        Invoke-Expression "`$in = `$node.`'$name`'"
-        if ($in.GetType().Name -eq 'PSCustomObject')
-        {
-            Find-RefObject -node $in -list $list
+
+    if ($node.GetType().Name -eq 'Object[]')
+    {
+        foreach ($item in $node) {
+            Find-RefObject -node $item -list $list
         }
     }
+    if ($node.GetType().Name -eq 'PSCustomObject')
+    { 
+        $m = Get-Member -InputObject $node -MemberType 'NoteProperty'
+        $m | ForEach-Object {
+            $name = $_.Name
+            Write-Debug ("-"+"."*2*$script:ind + "$name")
+            if ($name.Length -gt 0)
+            {
+                Invoke-Expression "`$in = `$node.`'$name`'"
+                Find-RefObject -node $in -list $list
+            }
+        }
+    }
+    $script:ind--
 }
 
 
@@ -29,6 +44,7 @@ function Get-ReferencedObjects {
     
     Write-Debug "BEGIN: Get-ReferencedObjects(obj=$obj)"
 
+    $script:ind = 0
     [System.Collections.ArrayList] $arr = [System.Collections.ArrayList]::new()
     #$arr = Find-RefObject -node $obj.Body -list $arr
     Find-RefObject -node $obj.Body -list $arr
