6033deca2aa78574623dcede239a68d3099a6b23
Kamil <kamil@nowinski.net>
2021-08-03 01:15:39 +0100

    Recognise managedPrivateEndpoint type #95

--- private/AdfObject.class.ps1
+++ private/AdfObject.class.ps1
@@ -83,7 +83,7 @@ class AdfObject {
         return $ofn
     }
 
-    static $AllowedTypes = @('integrationRuntime', 'pipeline', 'dataset', 'dataflow', 'linkedService', 'trigger', 'factory', 'managedVirtualNetwork', 'managedPrivateEndpoints')
+    static $AllowedTypes = @('integrationRuntime', 'pipeline', 'dataset', 'dataflow', 'linkedService', 'trigger', 'factory', 'managedVirtualNetwork', 'managedPrivateEndpoint')
 
     static AssertType ([string] $Type)
     {
