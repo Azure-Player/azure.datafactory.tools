8f5a9c57becc8bdc71c6cc322d74b6910d779472
Kamil <kamil@nowinski.net>
2021-10-22 16:31:48 +0100

    Test-Exceptions

--- public/Test-AdfCode.ps1
+++ public/Test-AdfCode.ps1
@@ -135,3 +135,16 @@ function Test-AdfCode {
 
     return $ErrorCount;
 }
+
+
+function Test-ErrorNoTermination {
+    Write-Error -Message 'Test error message with no termination' -ErrorAction 'Continue'
+}
+
+function Test-ErrorTermination {
+    Write-Error -Message 'Test error message with termination' -ErrorAction 'Stop'
+}
+
+function Test-Exception {
+    Throw 'Test error message'
+}
