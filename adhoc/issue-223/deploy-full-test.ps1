a4534039a551da9e3a503aeaca585d9afa4b9b40
Kamil Nowinski <kamil@nowinski.net>
2022-11-17 22:47:43 +0000

    #223 extended code with snapshot of second adf (after renaming trigger)

--- adhoc/issue-223/deploy-full-test.ps1
+++ adhoc/issue-223/deploy-full-test.ps1
@@ -14,17 +14,29 @@ param
 Install-Module -Name azure.datafactory.tools -Scope CurrentUser -Force
 Import-Module -Name azure.datafactory.tools
 
-#Run TestAdfCode function to validate JSON
+# Run TestAdfCode function to validate JSON
 $RootFolder = (.\adhoc\Get-RootPath.ps1)
-Test-AdfCode -RootFolder "$RootFolder"
+Test-AdfCode -RootFolder "$RootFolder\adf1"
 
-#Set deploy parameters
+$IncludeExcludeFilePath = "$RootFolder\publish-includeexclude-objects.txt"
+$DeleteNotInSource = $false
+
+# Set deploy parameters
 $opt = New-AdfPublishOption -FilterFilePath $IncludeExcludeFilePath
-$opt.DeleteNotInSource = $DeleteNotInSource
+$opt.DeleteNotInSource = $true
 $opt.StopStartTriggers = $true
 $opt.DeployGlobalParams = $false
-$opt.CreateNewInstance = $false
+$opt.CreateNewInstance = $true
 
-#Deploy to Data Factory
-Publish-AdfV2FromJson -RootFolder "$RootFolder" -ResourceGroupName "$ResourceGroupName" -DataFactoryName "$DataFactoryName" -Location "$Location" -Stage $Stage -Option $opt
+# init vars
+$SubscriptionName = "MVP"
+$ResourceGroupName = 'rg-devops-factory'
+$DataFactoryName = 'adf-kn-issue-223'
+$Location = 'northeurope'
+
+# Deploy to Data Factory - leg 1
+Publish-AdfV2FromJson -RootFolder "$RootFolder\adf1" -ResourceGroupName "$ResourceGroupName" -DataFactoryName "$DataFactoryName" -Location "$Location" -Stage $Stage -Option $opt
+
+# Deploy to Data Factory - leg 2
+Publish-AdfV2FromJson -RootFolder "$RootFolder\adf2" -ResourceGroupName "$ResourceGroupName" -DataFactoryName "$DataFactoryName" -Location "$Location" -Stage $Stage -Option $opt
 
