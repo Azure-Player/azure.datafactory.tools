c27ac22dc50a1136e65ee60ad7c2cf75d90f2d6d
Rich Sage <rich.sage@costacoffee.com>
2022-11-21 09:40:55 +0000

    Add JSON and CSV tests for issue #240.

---
+++ adhoc/issue-240-v2/test-240-v2.ps1
@@ -0,0 +1,20 @@
+#Import-Module ".\azure.datafactory.tools.psd1" -Force
+#Get-Module 
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
+$DebugPreference = 'Continue'
+
+. .\adhoc\~~Load-all-cmdlets-locally.ps1   # Load to this session
+
+$path = (.\adhoc\Get-RootPath.ps1)
+$adf = Import-AdfFromFolder -FactoryName 'my-factory-001' -RootFolder $path
+
+# Do test
+# Enable the relevant line based on the configuration being tested
+#Test-AdfCode -RootFolder $path -ConfigPath "$path\config.csv"
+#Test-AdfCode -RootFolder $path -ConfigPath "$path\config.json"
+
+# Do real update
+# Enable the relevant line based on the configuration being tested
+#Update-PropertiesFromFile -adf $adf -stage "$path\config.csv"
+#Update-PropertiesFromFile -adf $adf -stage "$path\config.json"
