b450cb1ddeaa6c50cd54e589fe95c51d30abbb2a
Kamil Nowinski <kamil@nowinski.net>
2023-07-10 22:16:30 +0100

    Added Unit tests for new global params #340

--- test/Update-PropertiesFromFile.Tests.ps1
+++ test/Update-PropertiesFromFile.Tests.ps1
@@ -262,6 +262,11 @@ InModuleScope azure.datafactory.tools {
                 $script:gp.Body.properties.globalParameters.'GP-Int'.value | Should -Be 2020
                 $script:gp.Body.properties.globalParameters.'GP-Bool'.value | Should -Be $False
             }
+            It 'Should contains new gp value added' {
+                $script:gp = Get-AdfObjectByName -adf $script:adf -name $script:adf.Factories[0].Name -type "factory"
+                $script:gp.Body.properties.globalParameters.'NewGlobalParam'.value | Should -Be 2023
+                $script:gp.Body.properties.globalParameters.'envName'.value | Should -Be "POC"
+            }
         }
     } 
 
